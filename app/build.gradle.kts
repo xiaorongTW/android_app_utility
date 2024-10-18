d90b0d95de0702cd81d85f0b96c66592184f00bf
clt\daniel_lin <50814738+xiaorongTW@users.noreply.github.com>
2024-10-19 01:21:44 +0800

    Include Google Gson Library
    [What] Include google gson library.
    [Why]. Support Json format.
    [How]. Include google gson library.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.kotlinx.coroutines.android)
+    implementation("com.google.code.gson:gson:2.8.8")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
